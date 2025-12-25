5dbb9cb9a7989f6f982274f3b897b467421ee496
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-25 17:19:46 +0500

    importing assets and constants

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,8 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Image, Text, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
 
 const _Layout = () => {
   return (
@@ -10,6 +12,23 @@ const _Layout = () => {
         options={{ 
           title: "Home",
           headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <>
+              <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 items-center justify-center rounded-full overflow-hidden"
+              >
+                <Image
+                  source={icons.home}
+                  tintColor="#151312"
+                  className="size-5"
+                />
+                <Text
+                  className="text-secondary text-base font-semibold ml-2"
+                >Home</Text>
+              </ImageBackground>
+            </>
+          ),
          }} 
       />
       <Tabs.Screen 
