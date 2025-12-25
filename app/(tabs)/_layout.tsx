665c5a3093b5c62b4bdd4be1d7b2432994c8a9a7
shanulabdin <muhammadshanulabedeen@gmail.com>
2025-12-25 17:31:19 +0500

    rendering using props

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,20 +4,20 @@ import { Tabs } from "expo-router";
 import { images } from "@/constants/images";
 import { icons } from "@/constants/icons";
 
-const TabIcon = () => {
+const TabIcon = ({ focused, icon, title }: any) => {
   return (
     <ImageBackground
       source={images.highlight}
       className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 items-center justify-center rounded-full overflow-hidden"
     >
       <Image
-        source={icons.home}
+        source={icon}
         tintColor="#151312"
         className="size-5"
       />
       <Text
         className="text-secondary text-base font-semibold ml-2"
-      >Home</Text>
+      >{title}</Text>
     </ImageBackground>
   );
 }
