33a40853a6de79e2a0b88c39c6e0439a983eb9da
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-01 21:37:45 +0500

    teaching some shit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,24 @@
 import { SearchBar } from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import { Image, ScrollView, View } from "react-native";
 import { useRouter } from "expo-router";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const searchPlaceholder = 'Search movies or TV shows';
 
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
 
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "120%", paddingBottom: 10 }}>
-        <Image source={icons.logo} className="w-12 h-12 mb-20 mt-20 mx-auto" />
+        <Image source={icons.logo} className="w-14 h-14 mb-20 mt-20 mx-auto" />
 
         <View className="mb-5 flex-1">
           <SearchBar 
             onPress={() => router.push('/search')}
-            placeholder="Search movies or TV shows"
+            placeholder={searchPlaceholder}
           />
         </View>
       </ScrollView>
