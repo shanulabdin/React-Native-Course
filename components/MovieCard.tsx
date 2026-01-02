27912c63af542d113f50d6c2ef3ebbd90cfc06fb
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-02 16:59:07 +0500

    fixed the error, now it shows images of the movies

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -5,12 +5,12 @@ import { Image, Text, TouchableOpacity } from "react-native"
 
 const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie ) => {
     return (
-        <Link href={`/movie/${id}`} asChild>
+        <Link href={`/movies/${id}`} asChild>
             <TouchableOpacity className="w-[30%]">
                 <Image 
                     source={{
                         uri: poster_path
-                            ? `https://image.tmdb.org/t/p/${poster_path}`
+                            ? `https://image.tmdb.org/t/p/w500${poster_path}`
                             : 'https://placehold.co/600x400/1a1a1a/ffffff.png'
                             
                     }}
