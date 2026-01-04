ee41c98d7541438c6260c04cba11c5a78750a140
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-04 21:15:34 +0500

    made the component for Trending movies and implemented it in the app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import MovieCard from "@/components/MovieCard";
 import { SearchBar } from "@/components/SearchBar";
+import TrendingCard from "@/components/TrendingCard";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
@@ -61,7 +62,10 @@ export default function Index() {
                 className="mb-4 mt-3"
                   data={trendingMovies}
                   renderItem={({ item, index }) => (
-                    <Text className="text-white text-sm ">{item.title}</Text>
+                    <TrendingCard 
+                      movie={item}
+                      index={index}
+                    />
                   )}
                   keyExtractor={(item) => item.movie_id.toString()}
                 />
