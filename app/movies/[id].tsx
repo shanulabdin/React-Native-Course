773f2306011cd6e8fc6b089c61618393cf3783ad
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-05 18:20:56 +0500

    added one movie info

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -59,6 +59,8 @@ const MovieDetails = () => {
               ({movie?.vote_count} votes)
             </Text>
           </View>
+          
+          <MovieInfo label="Overview" value={movie?.overview} />
         </View>
       </ScrollView>
     </View>
