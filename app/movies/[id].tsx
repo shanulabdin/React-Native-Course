c3df2f7a25bc55b3008be52e3c4647d274badf36
shanulabdin <muhammadshanulabedeen@gmail.com>
2026-01-05 18:17:08 +0500

    made a component for movie info

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -4,6 +4,22 @@ import useFetch from "@/services/useFetch";
 import { Stack, useLocalSearchParams } from "expo-router";
 import { Image, ScrollView, Text, View } from "react-native";
 
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className="flex-col items-start justify-center mt-5">
+    <Text className="text-light-200 font-normal text-sm">
+      {label}
+    </Text>
+    <Text className="text-light-100 font-bold text-sm mt-2">
+      {value || 'N/A'}
+    </Text>
+  </View>
+)
+
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
 
